f0fa60853308b45613a0a043e15c15b59943fba4
Jagdish Jat <jagdishjaipur11@rediffmail.com>
2014-04-13 19:52:48 +0530

    Update rtcamp.sh

--- rtcamp.sh
+++ rtcamp.sh
@@ -6,3 +6,28 @@
 sudo apt-get update
 
 #Check Which Software Installed Or not 
+#checking for Nginx and PHP and MYSql. If not Installed than Install
+PHP="php5"
+WS="nginx"
+DB="mysql"
+
+for pkg in $PHP $WS $DB; 
+do
+  if dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
+    echo -e "$pkg is already installed"
+  elif apt-get -qq install $pkg; then
+    echo "Successfully installed $pkg"
+  else
+    echo "Error installing $pkg"
+  fi
+done
+
+#User Input for Hostname 
+echo "Enter your hostname (Like : example.com) : \n"
+read hostname
+
+#Enter Details for hostname in /etc/hosts
+echo " 127.0.0.1     $hostname 
+     " > /ete/hosts
+
+#Configuration File for hostname of Nginx web Server
