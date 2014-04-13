8eab3d8291186283166fd1980f3835bee42d734e
Jagdish Jat <jagdishjaipur11@rediffmail.com>
2014-04-13 19:07:16 +0530

    Create rtcamp.sh

---
+++ rtcamp.sh
@@ -0,0 +1,8 @@
+#This is a shell Script tested on Ubuntu 12.04 LTS
+#Pre Requirements
+#Ubuntu 12.04 LTS Operating System
+
+#Update a System
+sudo apt-get update
+
+#Check Which Software Installed Or not 
