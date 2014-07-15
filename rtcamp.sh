75212f330e6adb3bcaba88b1593b5c9e6b45aa60
Jagdish Jat <jagdishjaipur11@rediffmail.com>
2014-07-15 14:59:51 +0530

    Update rtcamp.sh

--- rtcamp.sh
+++ rtcamp.sh
@@ -2,16 +2,21 @@
 #Pre Requirements
 #Ubuntu 12.04 LTS Operating System
 
+#find which OS install 
+OS="`lsb_release -i | cut -d':' -f2 | awk '{print $1}'`"
+
+if [ $OS != "Ubuntu" ];then
+echo "This scripts is created to Work on Ubuntu" 1>&2
+echo "Quiting..." 1>&2
+exit 1
+
 #Update a System
 sudo apt-get update
 sudo apt-get upgrade
 
-#Install Vim editor
-sudo apt-get install vim
-
 #Check Which Software Installed Or not 
 #checking for Nginx and PHP and MYSql. If not Installed than Install
-PHP="php5"
+PHP="php5-fpm"
 WS="nginx"
 DB="mysql-server"
 
@@ -28,19 +33,20 @@ done
 
 #User Input for Domain Name
 echo "Enter your domain (Like : example.com) : \n"
-read hostname
+read DOMAIN
 
 #Enter Details for hostname in /etc/hosts
-echo " 127.0.0.1     $hostname 
-     " > /ete/hosts
+echo " 127.0.0.1     $DOMAIN 
+     " >> /ete/hosts
 
 #Configuration File for hostname of Nginx web Server
 #Nginx server host file placed at /etc/nginx/sites-available/default
 
 #Create new Virtual Hostfile 
-sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/example.com
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$DOMAIN
 
 #Set up virtual Host
+echo "
 
 
 
